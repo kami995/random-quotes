888a082993c4077884a0411511a751adf7f861f1
Kami <mihailopetrovic995@gmail.com>
2020-04-11 15:35:22 +0200

    Updates

---
+++ precache-manifest.d00ef719f7a75d63e0aa7570231a7650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8474dbe1450e698e48b7939c2b1fce",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "ca13cbbbc4dd3c8e1f37",
+    "url": "/random-quotes/static/css/main.bf55f70c.chunk.css"
+  },
+  {
+    "revision": "d8808fbf03fc6ac20f2b",
+    "url": "/random-quotes/static/js/2.c6cfe515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quotes/static/js/2.c6cfe515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca13cbbbc4dd3c8e1f37",
+    "url": "/random-quotes/static/js/main.01d8ad9f.chunk.js"
+  },
+  {
+    "revision": "bbbfea294c973c555916",
+    "url": "/random-quotes/static/js/runtime-main.38e4d086.js"
+  }
+]);
